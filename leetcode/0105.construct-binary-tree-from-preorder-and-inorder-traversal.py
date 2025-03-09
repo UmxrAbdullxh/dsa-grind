89219b0ac12925d306aa8cf9cbe0ae42bc551583
UmxrAbdullxh <umarabdullah360@gmail.com>
2025-03-09 13:34:48 +0530

    solved construct binary tree from preorder and inorder

---
+++ leetcode/0105.construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+        # preorder first element is always root
+        root = TreeNode(preorder[0])
+        # get index of root in order to divide the left subtree and right subtree
+        mid = inorder.index(preorder[0])
+        # divide the left subtree and right subtree according to mid
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+        return root
+        
