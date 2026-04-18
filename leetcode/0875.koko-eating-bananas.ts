edb4f3b148661c1cf426703e8edc54e9de6946ea
UmxrAbdullxh <umarabdullah360@gmail.com>
2026-04-18 20:26:26 +0530

    solved koko eating bananas in ts

---
+++ leetcode/0875.koko-eating-bananas.ts
@@ -0,0 +1,20 @@
+function minEatingSpeed(piles: number[], h: number): number {
+    let l = 1
+    let r = Math.max(...piles)
+    let res = r
+    while(l <= r) {
+        let k = Math.floor((l+r) / 2)
+        let hours = 0
+        for(let p of piles) {
+            hours += Math.ceil(p/k) 
+        }
+        if(hours <= h) {
+            res = Math.min(res, k)
+            r = k - 1
+        }
+        else {
+            l = k + 1
+        }
+    }
+    return res
+};
