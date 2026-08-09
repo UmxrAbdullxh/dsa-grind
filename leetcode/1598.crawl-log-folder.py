bc6b882ad641191a67ac50a84541ef2c7b7f8c5e
UmxrAbdullxh <umarabdullah360@gmail.com>
2026-08-10 00:22:32 +0530

    solved using stack

---
+++ leetcode/1598.crawl-log-folder.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack = []
+        for i in logs:
+            if i == "../":
+                if len(stack) > 0:
+                    stack.pop()
+            else:
+                if i != "./":
+                    stack.append(i)
+        return len(stack)
+        
