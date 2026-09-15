8353e10c5ce768a67826f2460e341f49e546273e
UmxrAbdullxh <umarabdullah360@gmail.com>
2026-09-16 00:56:31 +0530

    solved with preorder

--- leetcode/0100.same-tree.py
+++ leetcode/0100.same-tree.py
@@ -8,24 +8,12 @@ class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if not p and not q:
             return True
-        queue1 = deque([p])
-        queue2 = deque([q])
-        while(queue1 and queue2):
-            node1 = queue1.popleft()
-            node2 = queue2.popleft()
-            if (node1 and not node2) or (node2 and not node1):
-                return False
-            if node1.val != node2.val:
-                return False
-            if(node1.left and not node2.left) or (node2.left and not node1.left):
-                return False
-            if (node1.right and not node2.right) or (node2.right and not node1.right):
-                return False
-            if node1.left and node2.left:
-                queue1.append(node1.left)
-                queue2.append(node2.left)
-            if node1.right and node2.right:
-                queue1.append(node1.right)
-                queue2.append(node2.right)
-            
-        return True
+        if not p and q:
+            return False
+        if p and not q:
+            return False
+        if p and q and p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
